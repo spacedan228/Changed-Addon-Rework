5c096e23b8803578d6f8d659451d8e1a3edd2140
0SeNiA0 <61203056+0SeNiA0@users.noreply.github.com>
2025-08-23 22:13:37 +0200

    SummonLightningGoal fixes & improvements

--- src/main/java/net/foxyas/changedaddon/entity/goals/exp9/SummonLightningGoal.java
+++ src/main/java/net/foxyas/changedaddon/entity/goals/exp9/SummonLightningGoal.java
@@ -1,9 +1,7 @@
 package net.foxyas.changedaddon.entity.goals.exp9;
 
-import net.ltxprogrammer.changed.entity.ChangedEntity;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.valueproviders.FloatProvider;
@@ -17,13 +15,10 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.EnumSet;
 import java.util.Random;
 
 @ParametersAreNonnullByDefault
 public class SummonLightningGoal extends Goal {
-
-    protected static final EnumSet<Flag> FLAGS = EnumSet.of(Flag.MOVE, Flag.JUMP, Flag.LOOK);
 
     protected final PathfinderMob holder;
     protected final IntProvider cooldownProvider;
@@ -79,9 +74,8 @@ public class SummonLightningGoal extends Goal {
     public void start() {
         lightnings = lightningCountProvider.sample(holder.getRandom());
         castDuration = castDurationProvider.sample(holder.getRandom());
-        holder.level.playSound(null, strikePos.x, strikePos.y, strikePos.z, SoundEvents.LIGHTNING_BOLT_THUNDER, SoundSource.WEATHER, 1, 1);
-
-        setFlags(FLAGS);
+        holder.level.playSound(null, holder.getX(), holder.getY(), holder.getZ(), SoundEvents.LIGHTNING_BOLT_THUNDER, SoundSource.WEATHER, 1, 1);
+        holder.getNavigation().stop();
     }
 
     @Override
@@ -92,18 +86,17 @@ public class SummonLightningGoal extends Goal {
         if(castDuration > 0){
             castDuration--;
 
+            holder.setDeltaMovement(Vec3.ZERO);
             holder.getLookControl().setLookAt(target);
 
-            if(holder.tickCount % 20 == 0) {
-                Vec3 holderPos = holder.position();
+            if(holder.tickCount % 2 == 0){
                 ((ServerLevel) level).sendParticles(ParticleTypes.ELECTRIC_SPARK,
-                        holderPos.x - 0.5, holderPos.y, holderPos.z - 0.5,
-                        25, 1, 0, 1, 0);
+                        holder.getX() - 0.5, holder.getY(), holder.getZ() - 0.5,
+                        25, 1, 0.1, 1, 0.5);
             }
+
             return;
         }
-
-        if(!getFlags().isEmpty()) getFlags().clear();
 
         Random random = holder.getRandom();
         if(strikePos == null){
@@ -115,23 +108,29 @@ public class SummonLightningGoal extends Goal {
             lightningDelay--;
 
             int gameTime = holder.tickCount;
-            if(gameTime % 20 == 0) ((ServerLevel)level).sendParticles(ParticleTypes.ELECTRIC_SPARK, strikePos.x - 1.5, strikePos.y, strikePos.z - 1.5,
-                    50, 3, 0, 3, 0);
-
+            if(gameTime % 2 == 0){
+                ((ServerLevel) level).sendParticles(ParticleTypes.ELECTRIC_SPARK, strikePos.x - 1, strikePos.y, strikePos.z - 1,
+                        50, 2, 0.2, 2, 0.5);
+            }
             if((gameTime + 10) % 40 == 0) level.playSound(null, strikePos.x, strikePos.y, strikePos.z, SoundEvents.LIGHTNING_BOLT_THUNDER, SoundSource.WEATHER, 0.5f, 1);
             return;
         }
 
-        LightningBolt lightning = EntityType.LIGHTNING_BOLT.create(level);
-        lightning.moveTo(strikePos);
-        lightning.setDamage(damageProvider.sample(random));
-        level.addFreshEntity(lightning);
-
-        if (holder instanceof ChangedEntity changedEntity) {
-            lightning.setCause((ServerPlayer) changedEntity.getUnderlyingPlayer());
-        }
+        lightnings--;
+        lightning(level, strikePos.x, strikePos.y, strikePos.z, damageProvider.sample(random));
+        lightning(level, strikePos.x + 0.75, strikePos.y, strikePos.z + 0.75, damageProvider.sample(random));
+        lightning(level, strikePos.x + 0.75, strikePos.y, strikePos.z - 0.75, damageProvider.sample(random));
+        lightning(level, strikePos.x - 0.75, strikePos.y, strikePos.z - 0.75, damageProvider.sample(random));
+        lightning(level, strikePos.x - 0.75, strikePos.y, strikePos.z + 0.75, damageProvider.sample(random));
 
         strikePos = null;
+    }
+
+    protected void lightning(Level level, double x, double y, double z, float damage){
+        LightningBolt lightning = EntityType.LIGHTNING_BOLT.create(level);
+        lightning.moveTo(x, y, z);
+        lightning.setDamage(damage);
+        level.addFreshEntity(lightning);
     }
 
     @Override
