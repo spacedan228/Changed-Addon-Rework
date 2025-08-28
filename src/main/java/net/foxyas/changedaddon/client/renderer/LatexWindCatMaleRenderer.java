f55667c5cfced9ccc8cf07fe85578d7c811a52be
Foxyas <119010731+Foxyas@users.noreply.github.com>
2025-08-27 22:41:28 -0300

    Update LatexWindCatMaleRenderer.java
    Fixed Armor Of Male Renderer Of Wind Cat

--- src/main/java/net/foxyas/changedaddon/client/renderer/LatexWindCatMaleRenderer.java
+++ src/main/java/net/foxyas/changedaddon/client/renderer/LatexWindCatMaleRenderer.java
@@ -7,17 +7,17 @@ import net.ltxprogrammer.changed.client.renderer.AdvancedHumanoidRenderer;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.TransfurCapeLayer;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWolfModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleCatModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
-public class LatexWindCatMaleRenderer extends AdvancedHumanoidRenderer<LatexWindCatMaleEntity, LatexWindCatMaleModel, ArmorLatexMaleWolfModel<LatexWindCatMaleEntity>> {
+public class LatexWindCatMaleRenderer extends AdvancedHumanoidRenderer<LatexWindCatMaleEntity, LatexWindCatMaleModel, ArmorLatexMaleCatModel<LatexWindCatMaleEntity>> {
 
     private static final ResourceLocation TEXTURE = ChangedAddonMod.textureLoc("textures/entities/latex_wind_cat_male");
 
     public LatexWindCatMaleRenderer(EntityRendererProvider.Context context) {
-        super(context, new LatexWindCatMaleModel(context.bakeLayer(LatexWindCatMaleModel.LAYER_LOCATION)), ArmorLatexMaleWolfModel.MODEL_SET, 0.5F);
+        super(context, new LatexWindCatMaleModel(context.bakeLayer(LatexWindCatMaleModel.LAYER_LOCATION)), ArmorLatexMaleCatModel.MODEL_SET, 0.5F);
         this.addLayer(TransfurCapeLayer.normalCape(this, context.getModelSet()));
         this.addLayer(new CustomEyesLayer<>(this, context.getModelSet()));
         this.addLayer(GasMaskLayer.forSnouted(this, context.getModelSet()));
