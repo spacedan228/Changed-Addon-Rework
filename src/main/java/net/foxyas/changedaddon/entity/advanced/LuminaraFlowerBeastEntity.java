727fb69ae54db1c4e2368a4d5d5f004b2845da8e
Foxyas <CarlosDeck9474@gmail.com>
2025-08-31 15:09:04 -0300

    luminara now apply the pacified effect

--- src/main/java/net/foxyas/changedaddon/entity/advanced/LuminaraFlowerBeastEntity.java
+++ src/main/java/net/foxyas/changedaddon/entity/advanced/LuminaraFlowerBeastEntity.java
@@ -3,16 +3,22 @@ package net.foxyas.changedaddon.entity.advanced;
 import net.foxyas.changedaddon.entity.advanced.handle.VoidTransformationHandler;
 import net.foxyas.changedaddon.entity.defaults.AbstractBasicOrganicChangedEntity;
 import net.foxyas.changedaddon.init.ChangedAddonEntities;
+import net.foxyas.changedaddon.init.ChangedAddonMobEffects;
 import net.foxyas.changedaddon.util.ColorUtil;
+import net.foxyas.changedaddon.util.FoxyasUtils;
 import net.foxyas.changedaddon.variants.VariantExtraStats;
 import net.ltxprogrammer.changed.entity.ChangedEntity;
 import net.ltxprogrammer.changed.entity.TransfurCause;
+import net.ltxprogrammer.changed.entity.variant.TransfurVariantInstance;
 import net.ltxprogrammer.changed.init.ChangedAttributes;
+import net.ltxprogrammer.changed.init.ChangedTags;
+import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
@@ -28,6 +34,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.PlayMessages;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -227,7 +234,7 @@ public class LuminaraFlowerBeastEntity extends AbstractBasicOrganicChangedEntity
         if (player != null) {
             if (this.isAwakened() && !attributesApplied) {
                 applyAwakenedBuffs();
-            } else if (!this.isAwakened() && attributesApplied){
+            } else if (!this.isAwakened() && attributesApplied) {
                 removeAwakenedBuffs();
             }
         }
@@ -236,6 +243,34 @@ public class LuminaraFlowerBeastEntity extends AbstractBasicOrganicChangedEntity
     @Override
     public void baseTick() {
         super.baseTick();
+
+        List<LivingEntity> nearChangedBeasts = this.getLevel().getEntitiesOfClass(LivingEntity.class, this.getBoundingBox().inflate(16), (entity) -> FoxyasUtils.canEntitySeeOther(entity, this));
+        for (LivingEntity livingEntity : nearChangedBeasts) {
+            if (livingEntity instanceof ChangedEntity changedEntity) {
+                if (changedEntity instanceof LuminaraFlowerBeastEntity) {
+                    continue;
+                }
+
+                if (changedEntity.getType().is(ChangedTags.EntityTypes.LATEX)) {
+                    if (!changedEntity.hasEffect(ChangedAddonMobEffects.PACIFIED.get())) {
+                        changedEntity.addEffect(new MobEffectInstance(ChangedAddonMobEffects.PACIFIED.get(), 60 * 20, 0, true, false, true));
+                    }
+                }
+            } else if (livingEntity instanceof Player player) {
+                TransfurVariantInstance<?> instance = ProcessTransfur.getPlayerTransfurVariant(player);
+                if (instance != null) {
+                    if ((instance.getChangedEntity() instanceof LuminaraFlowerBeastEntity)) {
+                        continue;
+                    }
+
+                    if (instance.getParent().getEntityType().is(ChangedTags.EntityTypes.LATEX)) {
+                        if (!player.hasEffect(ChangedAddonMobEffects.PACIFIED.get())) {
+                            player.addEffect(new MobEffectInstance(ChangedAddonMobEffects.PACIFIED.get(), 60 * 20, 0, true, false, true));
+                        }
+                    }
+                }
+            }
+        }
 
         if (this.isAwakened() && !attributesAppliedEntity) {
             this.setAttributesAwakened(this.getAttributes());
