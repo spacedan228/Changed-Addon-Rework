4b9dc641984324de69278e155e6e8505633d6105
Foxyas <CarlosDeck9474@gmail.com>
2025-09-19 14:19:54 -0300

    Update EntityOutlineLayer.java
    make the lines more smooth

--- src/main/java/net/foxyas/changedaddon/client/renderer/layers/EntityOutlineLayer.java
+++ src/main/java/net/foxyas/changedaddon/client/renderer/layers/EntityOutlineLayer.java
@@ -1,18 +1,15 @@
 package net.foxyas.changedaddon.client.renderer.layers;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.foxyas.changedaddon.client.renderer.renderTypes.ChangedAddonRenderTypes;
 import net.ltxprogrammer.changed.client.renderer.model.AdvancedHumanoidModel;
 import net.ltxprogrammer.changed.entity.ChangedEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import org.jetbrains.annotations.NotNull;
-import org.lwjgl.opengl.GL11;
 
 public class EntityOutlineLayer<M extends AdvancedHumanoidModel<T>, T extends ChangedEntity> extends RenderLayer<T, M> {
 
@@ -23,8 +20,8 @@ public class EntityOutlineLayer<M extends AdvancedHumanoidModel<T>, T extends Ch
     @Override
     public void render(@NotNull PoseStack poseStack, @NotNull MultiBufferSource bufferSource, int packedLight, @NotNull T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         poseStack.pushPose();
-        poseStack.scale(1.01f, 1.01f, 1.01f);
-        poseStack.translate(0, -0.005f, 0); // Small tweak in the outline value
+        poseStack.scale(1.01f, 1f, 1.01f);
+        poseStack.translate(0, 0, 0); // Small tweak in the outline value
 
         float r = 1f, g = 1f, b = 1f, a = 1f;
         VertexConsumer outlineBuffer = bufferSource.getBuffer(ChangedAddonRenderTypes.outlineWithDepth(this.getTextureLocation(entity)));
