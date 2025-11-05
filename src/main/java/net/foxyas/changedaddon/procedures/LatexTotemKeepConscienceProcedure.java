aa75426230857783102956fee2d59dd80c01eb6b
0SeNiA0 <61203056+0SeNiA0@users.noreply.github.com>
2025-11-05 22:42:33 +0100

    fix #132 (should be good enough unless someone will fill up the stack too much)

--- src/main/java/net/foxyas/changedaddon/procedures/LatexTotemKeepConscienceProcedure.java
+++ src/main/java/net/foxyas/changedaddon/procedures/LatexTotemKeepConscienceProcedure.java
@@ -1,7 +1,9 @@
 package net.foxyas.changedaddon.procedures;
 
 import net.foxyas.changedaddon.init.ChangedAddonItems;
+import net.ltxprogrammer.changed.block.WhiteLatexTransportInterface;
 import net.ltxprogrammer.changed.process.ProcessTransfur;
+import net.ltxprogrammer.changed.util.StackUtil;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerPlayer;
@@ -14,19 +16,19 @@ public class LatexTotemKeepConscienceProcedure {
 
     @SubscribeEvent
     public static void execute(ProcessTransfur.KeepConsciousEvent event) {
-        if (event.player == null)
-            return;
-        if (event.player.getInventory().contains(new ItemStack(ChangedAddonItems.TRANSFUR_TOTEM.get()))) {
-            if (!event.keepConscious) {
-                event.shouldKeepConscious = true;
-                if (event.player instanceof ServerPlayer serverPlayer) {
-                    TranslatableComponent text = new TranslatableComponent("changed_addon.latex_totem.tittle.text_1");
-                    TranslatableComponent text2 = new TranslatableComponent("changed_addon.latex_totem.tittle.text_2");
-                    serverPlayer.displayClientMessage(text, true);
-                    serverPlayer.sendMessage(text, ChatType.CHAT, serverPlayer.getUUID());
-                    serverPlayer.sendMessage(text2, ChatType.CHAT, serverPlayer.getUUID());
-                }
-            }
+        if (event.shouldKeepConscious
+                || event.player == null
+                || !event.player.getInventory().contains(new ItemStack(ChangedAddonItems.TRANSFUR_TOTEM.get()))) return;
+
+        if (ProcessTransfur.getPlayerTransfurVariant(event.player) != null && StackUtil.callStackContainsClass(WhiteLatexTransportInterface.class, 15)) return;
+
+        event.shouldKeepConscious = true;
+        if (event.player instanceof ServerPlayer serverPlayer) {
+            TranslatableComponent text = new TranslatableComponent("changed_addon.latex_totem.tittle.text_1");
+            TranslatableComponent text2 = new TranslatableComponent("changed_addon.latex_totem.tittle.text_2");
+            serverPlayer.displayClientMessage(text, true);
+            serverPlayer.sendMessage(text, ChatType.CHAT, serverPlayer.getUUID());
+            serverPlayer.sendMessage(text2, ChatType.CHAT, serverPlayer.getUUID());
         }
     }
 }
