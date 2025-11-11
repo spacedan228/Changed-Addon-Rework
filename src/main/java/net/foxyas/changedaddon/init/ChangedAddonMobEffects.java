08c670b29f26257cdd77d7e3bf239d13dd36b63a
0SeNiA0 <61203056+0SeNiA0@users.noreply.github.com>
2025-11-11 22:52:38 +0100

    update uk_ua.json

--- src/main/java/net/foxyas/changedaddon/init/ChangedAddonMobEffects.java
+++ src/main/java/net/foxyas/changedaddon/init/ChangedAddonMobEffects.java
@@ -8,7 +8,9 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ChangedAddonMobEffects {
+
     public static final DeferredRegister<MobEffect> REGISTRY = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, ChangedAddonMod.MODID);
+
     public static final RegistryObject<MobEffect> UNTRANSFUR = REGISTRY.register("untransfur", UntransfurMobEffect::new);
     public static final RegistryObject<MobEffect> LATEX_SOLVENT = REGISTRY.register("latex_solvent", LatexSolventMobEffect::new);
     public static final RegistryObject<MobEffect> LATEX_CONTAMINATION = REGISTRY.register("latex_contamination", LatexContaminationMobEffect::new);
