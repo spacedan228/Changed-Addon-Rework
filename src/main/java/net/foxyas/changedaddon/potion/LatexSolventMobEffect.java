6b01457caa76aff76b4bc4584b46be3e6eb6c467
Foxyas <CarlosDeck9474@gmail.com>
2025-11-20 16:57:22 -0300

    add annotation

--- src/main/java/net/foxyas/changedaddon/potion/LatexSolventMobEffect.java
+++ src/main/java/net/foxyas/changedaddon/potion/LatexSolventMobEffect.java
@@ -28,7 +28,7 @@ public class LatexSolventMobEffect extends MobEffect {
     }
 
     @Override
-    public void applyEffectTick(LivingEntity entity, int amplifier) {
+    public void applyEffectTick(@NotNull LivingEntity entity, int amplifier) {
         if (ProcessTransfur.getEntityVariant(entity).map(var -> var.getEntityType().is(ChangedTags.EntityTypes.LATEX)).orElse(false)) {
             entity.hurt(ChangedAddonDamageSources.LATEX_SOLVENT, amplifier + 2);
         }
