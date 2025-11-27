405583bca2e5b818738b6ff804f4014fdfcfbe49
Foxyas <CarlosDeck9474@gmail.com>
2025-11-27 16:36:41 -0300

    deleted temp ability tree

--- src/main/java/net/foxyas/changedaddon/datagen/ability_tree/AbilityTreeProviderImpl.java
+++ src/main/java/net/foxyas/changedaddon/datagen/ability_tree/AbilityTreeProviderImpl.java
@@ -24,33 +24,33 @@ public class AbilityTreeProviderImpl extends AbilityTreeProvider {
 
     @Override
     protected void addTrees() {
-        addTree(Changed.modResource("feline"), List.of(RegistryElementPredicate.forTag(ChangedRegistry.TRANSFUR_VARIANT.get(),
-                Changed.modResource("feline_light"))))
-                .withNode(Changed.modResource("root"), new AbilityTree.Node(
-                        AbilityTree.ROOT_NAME,             // parent
-                        List.of(),                         // occludes
-                        "ability.changed.root",            // titleId
-                        "ability.changed.root.desc",       // descriptionId
-                        0,                                 // price
-                        0,                                 // group discount
-                        List.of(),                         // acquiredEffects
-                        List.of()                          // missingEffects
-                ))
-                .withNode(Changed.modResource("claws"), new AbilityTree.Node(
-                        AbilityTree.ROOT_NAME,
-                        List.of(),
-                        "ability.changed.claws",
-                        "ability.changed.claws.desc",
-                        5,
-                        0,
-                        List.of(new AttributeModifierNodeEffect(
-                                        new StandingOnCondition(List.of(RegistryElementPredicate.forID(ForgeRegistries.BLOCKS, ForgeRegistries.BLOCKS.getKey(Blocks.GRASS_BLOCK)))),
-                                        ForgeMod.SWIM_SPEED.get(), 1
-                                ),
-                                new AttributeModifierNodeEffect(TrueCondition.INSTANCE, Attributes.ATTACK_DAMAGE, 0.5)
-                        )
-                        ,
-                        List.of()
-                ));
+//        addTree(Changed.modResource("feline"), List.of(RegistryElementPredicate.forTag(ChangedRegistry.TRANSFUR_VARIANT.get(),
+//                Changed.modResource("feline_light"))))
+//                .withNode(Changed.modResource("root"), new AbilityTree.Node(
+//                        AbilityTree.ROOT_NAME,             // parent
+//                        List.of(),                         // occludes
+//                        "ability.changed.root",            // titleId
+//                        "ability.changed.root.desc",       // descriptionId
+//                        0,                                 // price
+//                        0,                                 // group discount
+//                        List.of(),                         // acquiredEffects
+//                        List.of()                          // missingEffects
+//                ))
+//                .withNode(Changed.modResource("claws"), new AbilityTree.Node(
+//                        AbilityTree.ROOT_NAME,
+//                        List.of(),
+//                        "ability.changed.claws",
+//                        "ability.changed.claws.desc",
+//                        5,
+//                        0,
+//                        List.of(new AttributeModifierNodeEffect(
+//                                        new StandingOnCondition(List.of(RegistryElementPredicate.forID(ForgeRegistries.BLOCKS, ForgeRegistries.BLOCKS.getKey(Blocks.GRASS_BLOCK)))),
+//                                        ForgeMod.SWIM_SPEED.get(), 1
+//                                ),
+//                                new AttributeModifierNodeEffect(TrueCondition.INSTANCE, Attributes.ATTACK_DAMAGE, 0.5)
+//                        )
+//                        ,
+//                        List.of()
+//                ));
     }
 }
