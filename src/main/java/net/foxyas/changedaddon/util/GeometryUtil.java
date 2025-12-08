29436dcfc3f996a7a360cff78f59b75e564f9b48
0SeNiA0 <61203056+0SeNiA0@users.noreply.github.com>
2025-12-08 21:16:34 +0100

    added method to rotate VoxelShape around Y & a dummy screen

--- src/main/java/net/foxyas/changedaddon/util/GeometryUtil.java
+++ src/main/java/net/foxyas/changedaddon/util/GeometryUtil.java
@@ -1,7 +1,14 @@
 package net.foxyas.changedaddon.util;
 
+import net.minecraft.core.Direction;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Utility class for simple geometric checks such as circular or spherical area containment.
@@ -139,4 +146,38 @@ public class GeometryUtil {
                 point.z >= min.z && point.z <= max.z;
     }
 
+    public static @NotNull VoxelShape rotateShape(Direction direction, VoxelShape source) {
+        AtomicReference<VoxelShape> newShape = new AtomicReference<>(Shapes.empty());
+        source.forAllBoxes((minX, minY, minZ, maxX, maxY, maxZ) -> {
+            Vec3 min = new Vec3(minX - 0.5, minY - 0.5, minZ - 0.5);
+            Vec3 max = new Vec3(maxX - 0.5, maxY - 0.5, maxZ - 0.5);
+            Vec3 v1 = rotateVec3(min, direction);
+            Vec3 v2 = rotateVec3(max, direction);
+            VoxelShape s = Shapes.create(0.5 + Math.min(v1.x, v2.x), 0.5 + Math.min(v1.y, v2.y), 0.5 + Math.min(v1.z, v2.z),
+                    0.5 + Math.max(v1.x, v2.x), 0.5 + Math.max(v1.y, v2.y), 0.5 + Math.max(v1.z, v2.z));
+            newShape.set(Shapes.joinUnoptimized(newShape.get(), s, BooleanOp.OR));
+        });
+        return newShape.get();
+    }
+
+    public static @NotNull Vec3 rotateVec3(Vec3 vec, Direction dir) {
+        double cos = 1;
+        double sin = 0;
+        switch (dir) {
+            case SOUTH -> {
+                cos = -1;
+                sin = 0;
+            }
+            case WEST -> {
+                cos = 0;
+                sin = 1;
+            }
+            case EAST -> {
+                cos = 0;
+                sin = -1;
+            }
+        }
+
+        return new Vec3(vec.x * cos + vec.z * sin, vec.y, vec.z * cos - vec.x * sin);
+    }
 }
