333e5b40d7af1c4cd1dd55539bc9d2efa2a245cd
Foxyas <CarlosDeck9474@gmail.com>
2025-12-22 21:25:17 -0300

    Update TransfurVariantInstanceMixin.java

--- src/main/java/net/foxyas/changedaddon/mixins/entity/variant/TransfurVariantInstanceMixin.java
+++ src/main/java/net/foxyas/changedaddon/mixins/entity/variant/TransfurVariantInstanceMixin.java
@@ -19,6 +19,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -130,7 +131,12 @@ public abstract class TransfurVariantInstanceMixin implements TransfurVariantIns
         return this.abilityInstances.get(this.secondSelectedAbility);
     }
 
-    @Inject(method = "tickAbilities", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableCollection;iterator()Lcom/google/common/collect/UnmodifiableIterator;", shift = At.Shift.AFTER))
+    @Inject(method = "tickAbilities", at = @At(value = "FIELD",
+            target = "Lnet/ltxprogrammer/changed/entity/variant/TransfurVariantInstance;selectedAbility:Lnet/ltxprogrammer/changed/ability/AbstractAbility;",
+            ordinal = 0,
+            opcode = Opcodes.GETFIELD,
+            shift = At.Shift.BY)
+    )
     private void changedAddon$onTickAbilities(CallbackInfo ci) {
         if (ChangedAddonServerConfiguration.ALLOW_SECOND_ABILITY_USE.get()) {
             if (!this.isTemporaryFromSuit() && this.shouldApplyAbilities()) {
