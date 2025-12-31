28c560b84b92878ce329899cae0eacef090fbfe5
Foxyas <CarlosDeck9474@gmail.com>
2025-12-31 15:33:08 -0300

    final tweaks

--- src/main/java/net/foxyas/changedaddon/item/TranslatorItem.java
+++ src/main/java/net/foxyas/changedaddon/item/TranslatorItem.java
@@ -1,9 +1,14 @@
 package net.foxyas.changedaddon.item;
 
 import net.foxyas.changedaddon.init.ChangedAddonTabs;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class TranslatorItem extends Item {
 
@@ -14,6 +19,21 @@ public class TranslatorItem extends Item {
                 .tab(ChangedAddonTabs.CHANGED_ADDON_MAIN_TAB)
                 .stacksTo(64)
                 .rarity(Rarity.COMMON));
+    }
+
+    @Override
+    public @NotNull InteractionResultHolder<ItemStack> use(
+            Level level,
+            Player player,
+            @NotNull InteractionHand hand
+    ) {
+        ItemStack stack = player.getItemInHand(hand);
+
+        if (!level.isClientSide) {
+            toggle(stack);
+        }
+
+        return InteractionResultHolder.sidedSuccess(stack, level.isClientSide);
     }
 
     /* ===== STATE ===== */
