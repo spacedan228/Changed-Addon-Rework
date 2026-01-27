52bafcfc034c7f1787beeabe928e938e208cfcd4
Cseh Dominik <cseh.dominik05@gmail.com>
2026-01-27 07:38:56 +0100

    Update Hungarian Translation

--- src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
+++ src/main/java/net/foxyas/changedaddon/datagen/lang/HULanguageProvider.java
@@ -430,13 +430,18 @@ public class HULanguageProvider extends LanguageProvider {
 
         addGui("transfur_sounds_gui.bark", "Ugat");
         addGui("transfur_sounds_gui.chatter", "Nyeffeg");
-        addGui("transfur_sounds_gui.fox_scream", "Róka sikoly");
+        addGui("transfur_sounds_gui.dragon_growl", "Morog");
+        addGui("transfur_sounds_gui.dragon_roar", "Ordít");
+        addGui("transfur_sounds_gui.flop", "Csap");
+        addGui("transfur_sounds_gui.fox_scream", "Sikít");
         addGui("transfur_sounds_gui.growl", "Morog");
         addGui("transfur_sounds_gui.hiss", "Sziszeg");
         addGui("transfur_sounds_gui.howl", "Vonyít");
         addGui("transfur_sounds_gui.meow", "Nyávog");
+        addGui("transfur_sounds_gui.purr", "Dorombol");
         addGui("transfur_sounds_gui.purreow", "Dorombolva nyávog");
         addGui("transfur_sounds_gui.roar", "Ordít");
+        addGui("transfur_sounds_gui.swim", "Úszik");
         addGui("transfur_sounds_gui.whine", "Vinnyog");
         addGui("transfur_sounds_gui.yip", "Csahol");
         
