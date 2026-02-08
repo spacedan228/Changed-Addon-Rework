dce4afcfc95bcc3714b05c99e091cadde4255750
Foxyas <CarlosDeck9474@gmail.com>
2026-02-08 14:08:04 -0300

    Update DarkLatexWolfPupMixin.java
    made pups able to grow as alphas

--- src/main/java/net/foxyas/changedaddon/mixins/entity/changedEntity/DarkLatexWolfPupMixin.java
+++ src/main/java/net/foxyas/changedaddon/mixins/entity/changedEntity/DarkLatexWolfPupMixin.java
@@ -1,15 +1,20 @@
 package net.foxyas.changedaddon.mixins.entity.changedEntity;
 
 import net.foxyas.changedaddon.entity.api.IAlphaAbleEntity;
+import net.foxyas.changedaddon.init.ChangedAddonGameRules;
+import net.ltxprogrammer.changed.entity.ChangedEntity;
 import net.ltxprogrammer.changed.entity.beast.AbstractDarkLatexEntity;
 import net.ltxprogrammer.changed.entity.beast.AbstractLatexWolf;
 import net.ltxprogrammer.changed.entity.beast.DarkLatexWolfPup;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,9 +23,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = DarkLatexWolfPup.class, remap = false)
-public class DarkLatexWolfPupMixin extends AbstractDarkLatexEntity{
+public class DarkLatexWolfPupMixin extends AbstractDarkLatexEntity {
 
-    @Shadow protected int age;
+    @Shadow
+    protected int age;
 
     public DarkLatexWolfPupMixin(EntityType<? extends AbstractLatexWolf> p_19870_, Level p_19871_) {
         super(p_19870_, p_19871_);
@@ -29,9 +35,25 @@ public class DarkLatexWolfPupMixin extends AbstractDarkLatexEntity{
     @Inject(method = "applyCustomizeToAged", at = @At("TAIL"))
     private void syncAlphaData(AbstractDarkLatexEntity aged, CallbackInfo ci) {
         DarkLatexWolfPup self = (DarkLatexWolfPup) (Object) this;
+        if (level instanceof ServerLevel serverLevel) {
+            tryMakeItAlpha(serverLevel, aged);
+        }
+
         if (self instanceof IAlphaAbleEntity selfAlpha && aged instanceof IAlphaAbleEntity agedAlpha) {
             agedAlpha.setAlpha(selfAlpha.isAlpha());
             agedAlpha.setAlphaScale(selfAlpha.alphaAdditionalScale());
+        }
+    }
+
+    private void tryMakeItAlpha(ServerLevelAccessor pLevel, ChangedEntity changedEntity) {
+        if (changedEntity instanceof IAlphaAbleEntity iAlphaAbleEntity) {
+            boolean gamerule = pLevel.getLevel().getGameRules().getBoolean(ChangedAddonGameRules.DO_ALPHAS_SPAWN);
+            if (!gamerule) return;
+
+            float chance = iAlphaAbleEntity.chanceToSpawnAsAlpha();
+            if (random.nextFloat() <= chance) {
+                iAlphaAbleEntity.setAlpha(true);
+            }
         }
     }
 
